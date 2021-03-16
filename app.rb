b1f231944b4e0a69ee9160a8514339aa0618c652
savanna chiang <savannaac@gmail.com>
2021-03-15 21:51:00 -0500

    sinatra views lab

--- app.rb
+++ app.rb
@@ -3,6 +3,10 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+		"<h1>Hello World.</h1> <p>This HTML code is inside of a '.erb' file</p>"
 	end
+
+	get ("/") { erb :index}
+
+	get ("/info") { erb :info }	
 end
